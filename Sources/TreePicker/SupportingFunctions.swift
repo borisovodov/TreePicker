cad9328c3a669aeaab868e63180d201f194c1c22
Boris Ovodov <boris@ovodov.me>
2024-05-20 15:31:27 +0500

    Test ssh key commit sign.

---
+++ Sources/TreePicker/SupportingFunctions.swift
@@ -0,0 +1,22 @@
+//
+//  SupportingFunctions.swift
+//
+//
+//  Created by Boris Ovodov on 14.05.2024.
+//
+
+import Foundation
+
+func recursivelyHandleDataElementAndChildren<Data>(from parent: Data.Element, children childrenID: KeyPath<Data.Element, Data?>, action: (Data.Element) -> Void) where Data : RandomAccessCollection {
+    action(parent)
+    
+    guard let children = parent[keyPath: childrenID] else {
+        return
+    }
+    
+    for child in children {
+        recursivelyHandleDataElementAndChildren(from: child, children: childrenID) { child in
+            action(child)
+        }
+    }
+}
